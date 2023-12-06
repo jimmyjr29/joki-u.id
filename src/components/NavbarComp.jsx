ad876411a9a9e3860d46a3606b679819efafa8d2
unknown <fiverjims29@gmail.com>
2023-12-06 09:33:15 +0700

    fix navbar

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -4,14 +4,14 @@ import { NavLink } from "react-router-dom";
 
 const NavbarComp = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary fixed-top" style={{ height: "80px" }}>
+    <Navbar expand="lg" className="bg-body-tertiary fixed-top navbar-light bg-light ">
       <Container>
-        <Navbar.Brand href="#home" className="fw-bold">
+        <Navbar.Brand href="#home" className="fw-bold mt-1 mb-1">
           <Image src="./logo.png  " alt="Joki'U Logo" width="30" height="30" className="d-inline-block align-top mx-2" />
           Joki'U
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav ">
           <Nav className="mx-auto ">
             {navLinks.map((link) => {
               return (
